2b3d0f7d3cb8927c2f9264152bd51fc7203c06b8
Angel Sanchez <angel.s.p2064@gmail.com>
2025-02-20 14:51:40 +0100

    que culeo?

--- Front/src/js/pages/LogInPage/LogInPage.jsx
+++ Front/src/js/pages/LogInPage/LogInPage.jsx
@@ -1,8 +1,5 @@
-import CategoryScrollView from "../../components/dev/categoryScrollView/CategoryScrollView";
 import LoginForm from "./../../components/dev/login/LoginForm";
 
-import React from "react";
-import LoginForm from "../../components/dev/login/LoginForm";
 import "./LogInPage.css";
 const LogInPage = () => {
   return (
