1c07d30a1891fd4d13f8a33a8fdb8211246f450a
RicardoBarrioB <162453002+RicardoBarrioB@users.noreply.github.com>
2025-02-24 09:53:56 +0100

    update

--- Backend-API/routes/api.php
+++ Backend-API/routes/api.php
@@ -23,9 +23,9 @@ Route::middleware(['auth:sanctum'])
             });
         Route::controller(ProfileController::class)->group(function () {
             Route::prefix('users')->name('users.')->group(function () {
-                Route::get('', 'index');
+                Route::get('', 'index'); //muestra todos los usuarios
                 Route::post('store',  'store')->name('store');
-                Route::get('show/{user}', 'show')->name('show');
+                Route::get('show/{user}', 'show')->name('show'); //muestra el usuario por el id
                 Route::put('update/{user}', 'update')->name('update');
                 Route::put('changeRole/{user}', 'changeRole');
                 Route::delete('destroy/{user}', 'destroy');
@@ -51,49 +51,15 @@ Route::middleware(['auth:sanctum'])
         });
         Route::controller(PostController::class)->group(function () {
             Route::prefix('posts')->name('posts.')->group(function () {
-                Route::get('', 'index');
-                Route::get('showAll', 'showAll');
+                Route::get('', 'index'); // enseña los 10 últimos
+                Route::get('show', 'show'); // Enseña todos los posts
+                Route::get('show/{post}', 'showOne'); // Enseña un post por un id
+                Route::get('user/{id}', 'postUser');    //Enseña los post a traves del id del usuario
                 Route::post('store',  'store')->name('store');
-                Route::get('show/{role}', 'show')->name('show');
-                Route::put('update/{role}', 'update')->name('update');
-                Route::delete('destroy/{role}', 'destroy');
+                Route::put('update/{post}', 'update')->name('update');
+                Route::delete('destroy/{post}', 'destroy');
             });
         });
 
     });
-
-/*
-    Route::controller(ProfileController::class)->group(function () {
-        Route::get('', 'index')->name('index');
-        Route::post('store',  'store')->name('store');//->middleware(['auth'])->middleware(['role:administrador']);
-        Route::get('show/{user}', 'show')->name('show')->middleware(['auth']);
-        Route::put('update/{user}', 'update')->name('update')->middleware(['auth'])->middleware(['role:administrador']);
-        Route::put('changeRole/{user}', 'changeRole')->name('changeRole');//->middleware(['auth'])->middleware(['role:administrador']);
-        Route::delete('destroy/{user}', 'destroy')->name('destroy')->middleware(['auth'])->middleware(['role:administrador']);
-    });
     
-    Route::controller(CategoriesController::class)->group(function () {
-        Route::get('', 'index');
-        Route::post('store', 'store')->name('categories.store');//->middleware(['auth'])->middleware(['role:administrador']);
-        Route::get('show/{categories}', 'show')->name('categories.show');//->middleware(['auth']);
-        Route::put('update/{categories}', 'update')->name('categories.update');//->middleware(['auth'])->middleware(['role:administrador']);
-        Route::delete('destroy/{categories}', 'destroy')->name('categories.destroy');//->middleware(['auth'])->middleware(['role:administrador']);
-    });
-    
-    Route::controller(RoleController::class)->group(function () {
-        Route::get('/role', 'index');
-        Route::post('/role/store', 'store')->name('categories.store')->middleware(['auth'])->middleware(['role:administrador']);
-        Route::get('/role/show/{role}', 'show')->name('categories.show')->middleware(['auth']);
-        Route::put('/role/update/{role}', 'update')->name('categories.update')->middleware(['auth'])->middleware(['role:administrador']);
-        Route::delete('/role/destroy/{role}', 'destroy')->name('categories.destroy')->middleware(['auth'])->middleware(['role:administrador']);
-    });
-    
-    Route::controller(PostController::class)->group(function () {
-        Route::get('/posts', 'index')->name('posts.index');
-        Route::get('/posts/showAll', 'showAll');
-        Route::post('/posts/store', 'store')->name('posts.store')->middleware(['auth'])->middleware(['role:administrador']);
-        Route::get('/posts/show/{post}', 'show')->name('posts.show')->middleware(['auth']);
-        Route::put('/posts/update/{post}', 'update')->name('posts.update')->middleware(['auth'])->middleware(['role:administrador']);
-        Route::delete('/posts/destroy/{post}', 'destroy')->name('posts.destroy')->middleware(['auth'])->middleware(['role:administrador']);
-    });
-    */
