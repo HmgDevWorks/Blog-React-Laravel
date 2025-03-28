31ee574e46cef0ab83ae616b3f084af4de1b3e33
rbsnkrs13 <raulbenedid@hotmail.com>
2025-03-28 22:34:20 +0100

    factories tittle ES hecho(?)

--- Backend/database/factories/PostFactory.php
+++ Backend/database/factories/PostFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Factory as FakerFactory;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
@@ -16,15 +17,16 @@ class PostFactory extends Factory
      */
     public function definition(): array
     {
-        $status = fake()->randomElement(['published', 'draft', 'deleted'], [60, 30, 10]);
+        $faker = FakerFactory::create('es_ES');
+        $status = $faker->randomElement(['published', 'draft', 'deleted'], [60, 30, 10]);
 
         return [
-            'id_categories' => fake()->numberBetween(1, 10),
+            'id_categories' => $faker->numberBetween(1, 10),
             'user_id' => null, // Se asignará después
-            'title' => fake()->sentence(),
-            'content' => json_encode(['type' => 'yoopta', 'content' => fake()->paragraphs(3, true)]),
+            'title' => $faker->realText(rand(50,65)),  // Definir longitud del texto
+            'content' => json_encode(['type' => 'yoopta', 'content' => fake()->paragraph(3)]), // Genera solo un párrafo
             'status' => $status,
-            'views' => in_array($status, ['published', 'deleted']) ? fake()->numberBetween(0, 200) : 0, // Asigna vistas a published y deleted
+            'views' => in_array($status, ['published', 'deleted']) ? $faker->numberBetween(0, 200) : 0, // Asigna vistas a published y deleted
         ];
     }
 }
