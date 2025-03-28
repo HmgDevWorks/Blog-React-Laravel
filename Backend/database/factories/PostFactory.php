4b739ae3c5540c22051aef2cd03bf89ce877d95a
rbsnkrs13 <raulbenedid@hotmail.com>
2025-03-28 22:52:58 +0100

    factoria posts remoledadas 100%

--- Backend/database/factories/PostFactory.php
+++ Backend/database/factories/PostFactory.php
@@ -24,9 +24,17 @@ class PostFactory extends Factory
             'id_categories' => $faker->numberBetween(1, 10),
             'user_id' => null, // Se asignará después
             'title' => $faker->realText(rand(50,65)),  // Definir longitud del texto
-            'content' => json_encode(['type' => 'yoopta', 'content' => fake()->paragraph(3)]), // Genera solo un párrafo
+            'content' => json_encode([
+                'type' => 'yoopta',
+                'content' => fake()->paragraph(rand(5, 7), true) . "\n\n" .
+                            fake()->paragraph(rand(5, 7), true) . "\n\n" .
+                            fake()->paragraph(rand(5, 7), true) . "\n\n" .
+                            fake()->paragraph(rand(5, 7), true) . "\n\n" .
+                            fake()->paragraph(rand(5, 7), true) // Genera 5 párrafos largos
+            ]), // Genera solo un párrafo
             'status' => $status,
             'views' => in_array($status, ['published', 'deleted']) ? $faker->numberBetween(0, 200) : 0, // Asigna vistas a published y deleted
+            'created_at' => $faker->dateTimeBetween('2024-01-01', 'now')->format('Y-m-d H:i:s'), // Genera fecha aleatoria desde enero de 2024 hasta la fecha actual
         ];
     }
 }
