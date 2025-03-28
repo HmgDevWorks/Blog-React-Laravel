ff1d057ee13154892ae9e7a772aaa3c3c4d3444f
rbsnkrs13 <raulbenedid@hotmail.com>
2025-03-28 13:24:14 +0100

    update link password reset controller

--- Backend/app/Http/Controllers/Auth/PasswordResetController.php
+++ Backend/app/Http/Controllers/Auth/PasswordResetController.php
@@ -49,7 +49,7 @@ class PasswordResetController extends Controller
 
         //$user->notify(new PasswordResetNotification($token));
        // Mail::to($user->email_user)->send(new PasswordResetNotification($token)); 
-       $url = url(env('FRONTEND_URL') . '/password/reset/' . $token . '?email=' . $user->email_user); //creamos la url a mano con los datos que queremos mandar para que los recoja el front
+       $url = url(env('FRONTEND_URL') . '/login?token=' . $token . '&email=' . $user->email_user); //creamos la url a mano con los datos que queremos mandar para que los recoja el front
 
        Mail::to($user->email_user)->send(new PasswordResetMail($url,$user)); //funcion pa llamar al mail
 
